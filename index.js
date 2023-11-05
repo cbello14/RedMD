bde36efcb9bcd21c50372978bab3c64074e9935b
SebastienB <brandjsebastien@gmail.com>
2023-11-04 20:48:04 -0400

    Update index.js
    working sessionId

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ getID();
 */
 
 
-let url = 'https://endlessmedicalapi1.p.rapidapi.com/InitSession';
-const options = {
+var options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '8b8b68cd06msh507d5636302a77ap1a5367jsnb41ba4618d46',
@@ -26,16 +25,75 @@ const options = {
 	}
 };
 
+
 async function getID() {
+	let url = 'https://endlessmedicalapi1.p.rapidapi.com/InitSession';
 	try {
 		const response = await fetch(url, options);
 		const result = await response.text();
 		console.log(result);
-		var id = result.slice(28, result.length - 2);
-		console.log(id);
+		return JSON.parse(result).SessionID;
 	} catch (error) {
 		console.error(error);
 	}
 }
 
-getID();
+async function getOutcomes(){
+	const url = 'https://endlessmedicalapi1.p.rapidapi.com/GetOutcomes';
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'e049190567msh2f14db14023c908p1866afjsn026718c6294e',
+			'X-RapidAPI-Host': 'endlessmedicalapi1.p.rapidapi.com'
+		}
+	};
+
+	try {
+		const response = await fetch(url, options);
+		const result = await response.text();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function analyze() {
+	var it= await id;
+	console.log("^^^^")
+	console.log(id)
+	console.log("^^^^")
+	const url =  'https://endlessmedicalapi1.p.rapidapi.com/Analyze?SessionID='+it;
+	try {
+		const response = await fetch(url, options);
+		const result = await response.text();
+		console.log(result);
+	} catch (error) {
+		console.log("lmao")
+		console.error(error);
+	}
+}
+
+
+async function acceptTermsOfUse(){
+	var it=await id;
+	const url = 'https://endlessmedicalapi1.p.rapidapi.com/AcceptTermsOfUse?SessionID='+it+'&passphrase=I%20have%20read%2C%20understood%20and%20I%20accept%20and%20agree%20to%20comply%20with%20the%20Terms%20of%20Use%20of%20EndlessMedicalAPI%20and%20Endless%20Medical%20services.%20The%20Terms%20of%20Use%20are%20available%20on%20endlessmedical.com';
+	const options = {
+		method: 'POST',
+		headers: {
+			'X-RapidAPI-Key': 'e049190567msh2f14db14023c908p1866afjsn026718c6294e',
+			'X-RapidAPI-Host': 'endlessmedicalapi1.p.rapidapi.com'
+		}
+	};
+
+	try {
+		const response = await fetch(url, options);
+		const result = await response.text();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+const id=getID();
+acceptTermsOfUse();
+analyze();
